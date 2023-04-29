1c360226313243e1893e76564783f7bede69ab8f
James jpb <jpbefx@hotmail.com>
2023-04-29 00:08:35 -0500

    "screen size bug fix, + response-text readability"

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 Nicholas Werner, James Bailey, Larissa Passamani Lima
 CSD 460 - Red Team
 */
+
+/*
+Uses rome.js to check input_from and input_to, to make sure the from, and to dates
+ to validate range with dateValidator function. Check to assure
+  the input_from date is less than or equal to the input_to date.
+  time:boolean used to return result. 
+*/
 $(function() {
 
   rome(input_from, {
